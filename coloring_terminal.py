b475224bc839f6ee4dadcd6f13625445528613a1
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-11-23 22:42:13 +0530

    Add files via upload

---
+++ coloring_terminal.py
@@ -0,0 +1,13 @@
+from termcolor import colored
+print(colored("Hello,world!","yellow"))
+print(colored("Hello,world!","red"))
+print(colored("Hello,world!","cyan"))
+print(colored("Hello,world!","green"))
+print(colored("Hello,world!","grey"))
+print(colored("Hello,world!","blue"))
+print(colored("Hello,world!","magenta"))
+print(colored("Hello,world!","white"))
+
+
+
+
