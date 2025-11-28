ec166e1deb75e3fb77d3cf7ad0913346e81c12f0
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-11-28 23:39:23 +0530

    Add files via upload

---
+++ 15.practice.py
@@ -0,0 +1,5 @@
+#5. Print the remainder when one number is divided by another.
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+rem=float(num1 % num2)
+print(num1," % ",num2,"is = ",rem)
