4c7753730dacece67f83a2c3de27bae9a5cf06f3
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-11-29 18:45:36 +0530

    Add files via upload

---
+++ 16.practice.py
@@ -0,0 +1,7 @@
+#6. Check if a number is even or odd.
+while(True):
+    number=int(input("Enter the number: "))
+    if number %2 == 0 :
+        print("Number",number,"is even")
+    else:
+        print("Number",number,"is Odd")    
