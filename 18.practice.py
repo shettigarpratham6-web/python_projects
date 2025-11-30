e81063c9801be414122e7c6043147c1280d63808
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-11-30 22:15:45 +0530

    Add files via upload

---
+++ 18.practice.py
@@ -0,0 +1,14 @@
+#8. Input 3 numbers and print the largest.
+num1=int(input("Enter the value of A: "))
+num2=int(input("Enter the value of B: "))
+num3=int(input("Enter the value of C: "))
+max=num1
+if num2>max:
+    max=num2
+   
+if num3>max:
+    max=num3
+       
+print(max,"is greater")
+
+       
