8a32166d2282bb7ecddb73885057271d76d45208
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-12-01 19:36:38 +0530

    Add files via upload

---
+++ 21.practice.py
@@ -0,0 +1,4 @@
+#11. Input two strings and print them in a single line using f-string.
+string1=input("Enter the first string: ")
+string2=input("Enter the second string: ")
+print(f"{string1} {string2}")
