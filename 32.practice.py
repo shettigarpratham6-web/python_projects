51408192a43cef317cbbb2be600bbdaed2896b05
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-12-10 20:08:32 +0530

    Add files via upload

---
+++ 32.practice.py
@@ -0,0 +1,7 @@
+#22. Check if a character is vowel or consonant.
+char=input("Enter a Character: ")
+char2=char.lower()
+if char2=='a' or char2=='e' or char2=='i' or char2=='o'or char2=='u':
+    print(".....It is Vowel.....")
+else:
+    print(".....It is consonant.....")    
