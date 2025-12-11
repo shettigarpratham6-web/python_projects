4af679112548731b95d641ce4fde92ff08b8d0c7
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-12-11 07:12:06 +0530

    Add files via upload

---
+++ 33.practice.py
@@ -0,0 +1,32 @@
+#Usage of Bitwise AND Operator 
+print("Bitwise AND\n")
+a=int(input("Enter a number A: "))
+b=int(input("Enter a number B: "))
+c=a&b
+print(c)
+
+#Usage of Bitwise OR operator 
+print("Bitwise OR\n")
+a=int(input("Enter a number A: "))
+b=int(input("Enter a number B: "))
+c=a|b
+print(c)
+
+#Usage of Bitwise XOR operator 
+print("Bitwise XOR\n")
+a=int(input("Enter a number A: "))
+b=int(input("Enter a number B: "))
+c=a^b 
+print(c)
+
+#Usage of Bitwise NOT operator
+a=int(input("Enter a number A: "))  
+print(~a)
+
+#Usage of Bitwise Left Shift
+a=int(input("Enter a number: "))
+print(a<<1) #multiplies 2^n
+
+#Usage of Bitwise Right shift
+a=int(input("Enter a number: "))
+print(a>>1) #multiplies 2^n
